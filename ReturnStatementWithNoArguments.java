4394a44e1de46f9c0e47f58da098d96f4ddf93fa
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:04:19 +0530

    Add files via upload

---
+++ ReturnStatementWithNoArguments.java
@@ -0,0 +1,15 @@
+package statements;
+
+public class ReturnStatementWithNoArguments {
+
+	 static void myMethod() {
+		    System.out.println("I just got executed!");
+		  }
+
+		  public static void main(String[] args) {
+		    myMethod();
+
+	
+	}
+
+}
