794390ec84e7fec4b5e9b52e4403845716326bc8
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:34:11 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,15 @@
+package statements;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+	
+		 int number = 10;
+		    if (number > 0) 
+		    {
+		      System.out.println("The number is positive.");
+		    }
+		    System.out.println("Statement outside if block");
+	}
+
+}
