48b12f210003d2e3e5c4330fd1574ead7e3cdde0
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:35:03 +0530

    Add files via upload

---
+++ IfElseIfStatement.java
@@ -0,0 +1,24 @@
+package statements;
+
+public class IfElseIfStatement {
+
+	public static void main(String[] args) {
+		
+		int number = 0;
+	    if (number > 0) 
+	    {
+	      System.out.println("The number is positive.");
+	    }
+	    else if (number < 0) 
+	    {
+	      System.out.println("The number is negative.");
+	    }
+	    else 
+	    {
+	      System.out.println("The number is 0.");
+
+	    }
+
+	}
+
+}
