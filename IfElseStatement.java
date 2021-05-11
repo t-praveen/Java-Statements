b21f76346b26fefc694fa3e05a7a5af53ef2739e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:34:38 +0530

    Add files via upload

---
+++ IfElseStatement.java
@@ -0,0 +1,19 @@
+package statements;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+
+		 	int number = 10;
+		    if (number > 0) 
+		    {
+		      System.out.println("The number is positive.");
+		    }
+		    else 
+		    {
+		      System.out.println("The number is not positive.");
+		    }
+		    System.out.println("Statement outside if...else block");
+	}
+
+}
