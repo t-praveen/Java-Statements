d72f3bb593eec0c19d2a05eccfeda2edb4d533e6
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:50:02 +0530

    Add files via upload

---
+++ SiwtchWithoutBreak.java
@@ -0,0 +1,35 @@
+package statements;
+
+public class SiwtchWithoutBreak {
+
+	public static void main(String[] args) {
+
+		 int number = 44;
+		    String size;
+		    switch (number) 
+		    {
+		      case 29:
+		        size = "Small";
+		        System.out.println("Size: " + size);
+		      case 42:
+		        size = "Medium";
+		        System.out.println("Size: " + size);
+		      case 44:
+		        size = "Large";
+		        System.out.println("Size: " + size);
+		      case 48:
+		        size = "Extra Large";
+		        System.out.println("Size: " + size);	      
+		      default :
+		        size = "Unknown";
+		        System.out.println("Size: " + size);
+
+		        
+
+		    }
+		   
+		
+
+	}
+
+}
