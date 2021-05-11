4c84a6ffb291a37094fe06f4ee75e4742856587d
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 16:29:05 +0530

    Add files via upload

---
+++ SumOfPositiveNumbersUsingDoWhileLoop.java
@@ -0,0 +1,24 @@
+package statements;
+import java.util.Scanner;
+
+public class SumOfPositiveNumbersUsingDoWhileLoop {
+
+	public static void main(String[] args) {
+		
+			int sum = 0;
+		    int number = 0;
+		    Scanner input = new Scanner(System.in);
+		    do {
+		      sum += number;
+		      System.out.print("Enter a number : ");
+		      number = input.nextInt();
+		    } while(number >= 0); 
+			   
+		    System.out.println("Sum = " + sum);
+		    input.close();
+
+		
+
+	}
+
+}
