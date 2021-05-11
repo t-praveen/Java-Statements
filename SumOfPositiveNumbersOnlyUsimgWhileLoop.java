6ef430a98dc039fa507eefbc83e8b8a11bbd5a26
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 16:20:22 +0530

    Add files via upload

---
+++ SumOfPositiveNumbersOnlyUsimgWhileLoop.java
@@ -0,0 +1,24 @@
+package statements;
+import java.util.Scanner;
+
+public class SumOfPositiveNumbersOnlyUsimgWhileLoop {
+
+	public static void main(String[] args) {
+
+		   
+	    int sum = 0;
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("Enter a number : ");
+	    int number = input.nextInt();
+	    while (number >= 0) 
+	    {
+	      sum += number;
+
+	      System.out.print("Enter a number : ");
+	      number = input.nextInt();
+	    }
+	    System.out.println("Sum = " + sum);
+	    input.close();	
+	}
+
+}
