6624e03edbfb8675ab4df12d96db82aeb4e89fca
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-11 15:04:32 +0530

    Add files via upload

---
+++ ReturnStatementWithArguments.java
@@ -0,0 +1,21 @@
+package statements;
+
+public class ReturnStatementWithArguments {
+	
+	public int add(int x, int y) { // with arguments
+	      int z = x+y;
+	      return z;
+	}
+	
+
+	public static void main(String[] args) {
+		
+		  ReturnStatementWithArguments test = new ReturnStatementWithArguments();
+	      int add = test.add(10, 20);
+	      System.out.println("The sum of x and y is: " + add);
+
+		
+
+	}
+
+}
