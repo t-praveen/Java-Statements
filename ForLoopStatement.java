b50785f3ffb33609a2312aa469f5d15ba37b146b
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 14:00:13 +0530

    Add files via upload

---
+++ ForLoopStatement.java
@@ -0,0 +1,16 @@
+package statements;
+
+public class ForLoopStatement {
+
+	public static void main(String[] args) {
+		
+		    for (int i = 1; i <= 5; ++i) 
+		    {
+		      System.out.println("Java is fun");
+		    }
+
+		
+
+	}
+
+}
