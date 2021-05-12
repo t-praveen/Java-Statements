10b632251e27d7344d18e7ee14e38d48d311ca8c
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 15:14:30 +0530

    Add files via upload

---
+++ ContinueStatementExample2.java
@@ -0,0 +1,25 @@
+package statements;
+import java.util.Scanner;
+
+public class ContinueStatementExample2 {
+
+	public static void main(String[] args) {
+
+		 Double number, sum = 0.0;
+		    Scanner input = new Scanner(System.in);
+
+		    for (int i = 1; i < 6; ++i) 
+		    {
+		      System.out.print("Enter number " + i + " : ");
+		      number = input.nextDouble();
+		      if (number <= 0.0) 
+		      {
+		        continue;
+		      }
+		      sum += number;
+		    }
+		    System.out.println("Sum = " + sum);
+		    input.close();	
+	}
+
+}
