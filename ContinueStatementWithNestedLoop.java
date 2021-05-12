e5f17bb1fedf3ea84865f1f7b2d42686001d8444
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 15:14:47 +0530

    Add files via upload

---
+++ ContinueStatementWithNestedLoop.java
@@ -0,0 +1,29 @@
+package statements;
+
+public class ContinueStatementWithNestedLoop {
+
+	public static void main(String[] args) {
+
+		int i = 1, j = 1;
+	    while (i <= 3) 
+	    {
+
+	      System.out.println("Outer Loop: " + i);
+	      while(j <= 3)
+	      {
+
+	        if(j == 2) 
+	        {
+	          j++;
+	          continue;
+	        }
+	        System.out.println("Inner Loop: " + j);
+	        j++;
+	      }
+	      i++;
+	    }
+
+		
+	}
+
+}
