76abf8b5c9761946237ddbb899597defe45fb701
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 14:00:25 +0530

    Add files via upload

---
+++ ForLoopStatementExample2.java
@@ -0,0 +1,20 @@
+package statements;
+
+public class ForLoopStatementExample2 {
+
+	public static void main(String[] args) {
+		
+		  int sum = 0;
+		    for (int i = 1; i <= 1000; ++i)
+{
+		      sum += i;     
+		    }
+		       
+		    System.out.println("Sum = " + sum);
+
+		
+		
+		
+	}
+
+}
