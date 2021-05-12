1f916820c6362cfd891c1e9a7fc529432f902360
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 14:21:35 +0530

    Add files via upload

---
+++ ForEachLoopStatement.java
@@ -0,0 +1,19 @@
+package statements;
+
+public class ForEachLoopStatement {
+
+	public static void main(String[] args) {
+		
+		
+	    int[] numbers = {3, 9, 5, -5};
+
+	    for (int number: numbers) 
+	    {
+	      System.out.println(number);
+	    }
+
+		
+
+	}
+
+}
