5964978adc5a99613e1eba1f2a7945bb0479a14a
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 14:22:44 +0530

    Add files via upload

---
+++ ForEachLoopStatementExample2.java
@@ -0,0 +1,16 @@
+package statements;
+
+public class ForEachLoopStatementExample2 {
+
+	public static void main(String[] args) {
+		
+		   int[] numbers = {3, 4, 5, -5, 0, 12};
+		   int sum = 0;
+		   for (int number: numbers)
+		   {
+		     sum += number;
+		   }
+		   System.out.println("Sum = " + sum);
+	}
+
+}
