ee85b48c7973652751f0d8196dd2140e4734bcaf
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 14:37:52 +0530

    Add files via upload

---
+++ InfiniteForLoopStatement.java
@@ -0,0 +1,15 @@
+package statements;
+
+public class InfiniteForLoopStatement {
+
+	public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; --i) 
+        {
+            System.out.println("Hello");
+        }
+
+		
+	}
+
+}
