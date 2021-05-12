fbaabbaa8c5547d2f5d59b55088c71443e78ba93
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-12 15:14:17 +0530

    Add files via upload

---
+++ ContinueStatement.java
@@ -0,0 +1,17 @@
+package statements;
+
+public class ContinueStatement {
+
+	public static void main(String[] args) {
+	
+		 for (int i = 1; i <= 10; ++i) 
+		 {
+		      if (i > 4 && i < 9)
+		      {
+		        continue;
+		      }
+		      System.out.println(i);
+
+		 }
+	}
+}
